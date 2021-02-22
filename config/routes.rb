20ed83d17c0442b85c46005c7955337c79555c7f
Mariela Hernandez <mariela.hdz@berkeley.edu>
2021-02-21 20:37:38 -0800

    add temporary home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # devise_token_auth setup
   #mount_devise_token_auth_for 'User', at: '/api/v1'
-  
+
+  root "users#index"
+
   namespace :api do
     namespace :v1 do 
         resources :users
