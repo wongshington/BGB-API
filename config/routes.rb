eb52562fbc011ac0302ce429bb843ea2be1e6ff6
Mariela Hernandez <mariela.hdz@berkeley.edu>
2021-02-21 20:54:26 -0800

    add temporary splash page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # devise_token_auth setup
   #mount_devise_token_auth_for 'User', at: '/api/v1'
 
-  root "users#index"
+  root "user#index"
 
   namespace :api do
     namespace :v1 do 
