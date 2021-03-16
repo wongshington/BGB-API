50c8d4802cc9e76d6dee91842e2f7d2846e2981c
isabelgerrard <izzy2022@berkeley.edu>
2021-03-16 14:53:55 -0700

    pushing resources and topics table for natasha

--- app/models/connector.rb
+++ app/models/connector.rb
@@ -2,9 +2,14 @@
 #
 # Table name: connectors
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id             :integer          not null, primary key
+#  resource_id_id :integer
+#  topic_id_id    :integer
+#
+# Indexes
+#
+#  index_connectors_on_resource_id_id  (resource_id_id)
+#  index_connectors_on_topic_id_id     (topic_id_id)
 #
 class Connector < ApplicationRecord
   belongs_to :resource
